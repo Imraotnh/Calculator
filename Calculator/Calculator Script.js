8a3995a5d9e0823f47651d1054ca64503fa1d32c
Karakehayov <nkar@mail.bg>
2014-08-16 05:52:24 +0300

    Functionality almost ready

--- Calculator/Calculator Script.js
+++ Calculator/Calculator Script.js
@@ -1,3 +1,43 @@
-﻿console.log(17);
-var izraz = "3+6/2 + 2.4/3 - 2^3";
-console.log(eval(izraz));
+﻿$.fn.calculator = function () {
+    var $this = this,
+		$parentNode = $this;
+
+    var expression = "";
+    var notNumbers = ["+", "-", "/", "*", "%", ".", "±"];
+
+    jQuery(".regularButton").add(".widerButton").add(".higherButton").click(function () {
+        var $target = $(this);
+        console.log($target.text())
+
+        if ($target.text() !== "C" && $target.text() !== "=" && $target.text() !== "±") {
+            if ($.inArray($target.text(), notNumbers) !== -1 && $.inArray(expression.slice(-1), notNumbers) !== -1) {
+                alert("Not allowed!");
+            }
+
+            else {
+                expression += $target.text();
+            }
+        }
+
+        else if ($target.text() === "C") {
+            expression = "";
+        }
+
+        else if ($target.text() === "±") {
+            if (expression.slice(0, 1) === "-") {               
+                expression = expression.substring(1);
+            }
+
+            else {
+                expression = "-" + expression;
+            }
+        }
+
+        else {
+            var result = eval(expression).toString();
+            expression = result;
+        }
+
+        $(".result").val(expression);
+    });
+}
